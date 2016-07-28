9b119ebef4973ab630b042b7b54bae089adc73d4
ThiagoYonamine <thyonamine1@gmail.com>
2016-07-28 12:14:51 -0300

    testes Thiago

--- site2016/views.py
+++ site2016/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'site2016/home.html', {'nomes': ['Marcelo', 'Thiago', 'Fernando']})
+    return render(request, 'site2016/thiagoTeste.html', {'nomes': ['Marcelo', 'Thiago', 'Fernando']})
 
 def exemplo_foundation(request):
     return render(request, 'site2016/ExemplosFoundation/index.html', {})
