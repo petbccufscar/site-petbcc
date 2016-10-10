2656624a7b4b16db7eda7eaa5b5fb1dbc5174493
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2016-10-10 19:43:50 -0300

    Menu principal agora indica link atualmente ativo

--- site2016/views.py
+++ site2016/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'site2016/home.html', {})
-
+    context_dictionary = {'pagina': 'home'}
+    return render(request, 'site2016/home.html', context_dictionary)
 
 def equipe(request):
-    return render(request, 'site2016/equipe.html', {})
+    context_dictionary = {'pagina': 'equipe'}
+    return render(request, 'site2016/equipe.html', context_dictionary)
 
 def projetos(request):
-    return render(request, 'site2016/projetos.html', {})
+    context_dictionary = {'pagina': 'projetos'}
+    return render(request, 'site2016/projetos.html', context_dictionary)
 
 def exemplo_foundation(request):
     return render(request, 'site2016/ExemplosFoundation/index.html', {})
