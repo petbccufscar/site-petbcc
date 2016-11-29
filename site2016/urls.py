a9f8dcd0607d4e30ee413640ceebbc96285760f9
marcelodeolive1ra <marcelodeoliveira@outlook.com>
2016-11-29 13:45:14 -0200

    Implementado um "modo manutenção" no site

--- site2016/urls.py
+++ site2016/urls.py
@@ -3,10 +3,11 @@ from site2016 import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^manutencao/$', views.manutencao, name='manutencao'),
     url(r'^equipe/$', views.equipe, name='equipe'),
     url(r'^projetos/$', views.projetos, name='projetos'),
     # url(r'^noticias/$', views.noticias, name='noticias'),
     url(r'^sobre/$', views.sobre, name='sobre'),
     url(r'^processoseletivo/$', views.processo_seletivo, name='processo_seletivo'),
-    url(r'^contato/$', views.contato, name='contato')
+    url(r'^contato/$', views.contato, name='contato'),
 ]
