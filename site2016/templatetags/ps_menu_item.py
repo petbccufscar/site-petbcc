2a7e24e68521490ad67e1f3a3e3783740fcb4166
olivato <olivatooo@gmail.com>
2019-05-02 11:11:31 -0300

    Adicionado nova foto de equipe

--- site2016/templatetags/ps_menu_item.py
+++ site2016/templatetags/ps_menu_item.py
@@ -8,15 +8,20 @@ register = template.Library()
 @register.simple_tag
 def ps_menu():
     s = ""
-    for ps in ProcessoSeletivo.objects.order_by('-data_inscricao_inicio'):
-        s += '<a class="item" href="/processoseletivo/{0}/{1}">Processo Seletivo {0}/{1}</a>\n'.format(ps.ano,
+    try:
+        for ps in ProcessoSeletivo.objects.order_by('-data_inscricao_inicio'):
+            s += '<a class="item" href="/processoseletivo/{0}/{1}">Processo Seletivo {0}/{1}</a>\n'.format(ps.ano,
                                                                                                        ps.semestre)
+    except:
+        s = '<a class="item" href="#">Não há processos seletivos</a>'
     return mark_safe(s)
 
 
 @register.simple_tag
 def ps_link_atual():
-    ps = ProcessoSeletivo.objects.order_by('-data_inscricao_inicio')[0]
-
-    s = '<a href="/processoseletivo/{0}/{1}">Processo Seletivo</a>\n'.format(ps.ano, ps.semestre)
+    try:
+        ps = ProcessoSeletivo.objects.order_by('-data_inscricao_inicio')[0]
+        s = '<a href="/processoseletivo/{0}/{1}">Processo Seletivo</a>\n'.format(ps.ano, ps.semestre)
+    except:
+        s = '<a class="item" href="#">Não há processos seletivos</a>'
     return mark_safe(s)
